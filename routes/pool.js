7d0827a4a129fc7077ac783c68d1bde00ab9f882
SpvaigPvtLtd <info@spvaig.in>
2021-06-14 10:23:50 +0530

    a

--- routes/pool.js
+++ routes/pool.js
@@ -7,7 +7,7 @@ const pool = mysql.createPool({
  host : 'db-mysql-blr1-05486-do-user-9022348-0.b.db.ondigitalocean.com',
    user: 'doadmin',
     password : 'm8wcz9akv2dbl1jy',
-    database: 'dailynourish',
+    database: 'pureeasy',
     port:'25060' ,
     multipleStatements: true
   })
